c62a9ab5fd2c94e396e759f0ce61cf5b08d5a96b
MaccccD <Dumisanimofolo@gmail.com>
2025-10-06 15:59:52 +0200

    Game Scene | Movement & Code System Logic Update Part 1
    -more

--- Tek-Solve-Game/Assets/Scripts/MovementSystem.cs
+++ Tek-Solve-Game/Assets/Scripts/MovementSystem.cs
@@ -3,5 +3,19 @@ using Mirror;
 
 public class MovementSystem : NetworkBehaviour
 {
-   
+    [SyncVar] public Vector2Int player1Position;
+    [SyncVar] public Vector2Int player2Position;
+    [SyncVar] public MoveType lastMoveType;
+
+
+    [Command]
+
+    void CmdMove(int playerID, Vector2Int direction)
+    {
+        if(ValidateMove(playerID, direction))
+        {
+            ExecuteMove(playerID, direction);
+           
+        }
+    }
 }

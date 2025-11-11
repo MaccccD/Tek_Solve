77a950a1b674f8ab0b2aafb554cbb5b7fce94290
MaccccD <Dumisanimofolo@gmail.com>
2025-11-11 17:40:40 +0200

    Game Scene | Triggering Turns Update

--- Tek-Solve-Game/Assets/Scripts/UISystem.cs
+++ Tek-Solve-Game/Assets/Scripts/UISystem.cs
@@ -160,11 +160,18 @@ public class UISystem : MonoBehaviour
        // roundSystem.StartNextRound();//references to the grid and all the movement positions.(server only )
         roundsNumberTxt.text = "Round Number: " +  roundSystem.currentRound.ToString();// show the number of rounds.
         targetNumberTxt.text = "Target Number: " + gridSystem.targetNumber.ToString();// show the target number.
+       // turnSystemPanel.gameObject.SetActive(true);
+      //  DeactivateTurnSystemPanel();
+      //  turnSystemTxt.text = $"Player  {turnSystem.currentPlayerTurn}'s Turn";
+
+
+    }
+
+    public void TriggerTurnSystem()
+    {
         turnSystemPanel.gameObject.SetActive(true);
         DeactivateTurnSystemPanel();
         turnSystemTxt.text = $"Player  {turnSystem.currentPlayerTurn}'s Turn";
-
-
     }
 
     public void DisplayGridNumbers(int[,] grid)
